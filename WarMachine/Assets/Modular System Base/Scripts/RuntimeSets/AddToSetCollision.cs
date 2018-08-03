635f9def16a648935d07e2b57697a65e75a5e793
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2018-08-03 11:03:58 -0500

    Upload Project Base and Modular Start System
    Created A GameObject Spawner For Modular Start
    
    In Example used of the spawner in start and update by a gameEvent

---
+++ WarMachine/Assets/Modular System Base/Scripts/RuntimeSets/AddToSetCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using WARMachine.Sets.Things;
+using UnityEngine;
+
+public class AddToSetCollision : MonoBehaviour
+{
+    public ThingRuntimeSet Set;
+
+    public void Start()
+    {
+        Set.items.Clear();//Cheap hack to get rid of that pesky 1 that adds up        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Thing thing = other.gameObject.GetComponent<Thing>();
+        if (thing != null)
+        {
+            Debug.Log(other.gameObject.name + " This was added to  " + this.gameObject.name.ToString());
+            Set.Add(thing);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Thing thing = other.gameObject.GetComponent<Thing>();
+        if (thing != null)
+        {
+            Debug.Log(other.gameObject.name + " This was Removed from  " + this.gameObject.name.ToString());
+            Set.Remove(thing);
+        }
+    }
+
+}
---
+++ WarMachine/Assets/Modular System Base/Scripts/RuntimeSets/ThingMonitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WARMachine.Sets.Things
+{
+
+    public class ThingMonitor : MonoBehaviour
+    {
+        public ThingRuntimeSet Set;
+
+        public Text Text;
+
+        private int previousCount = -1;
+
+        // Use this for initialization
+        private void OnEnable()
+        {
+            UpdateText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (previousCount != Set.items.Count)
+            {
+                UpdateText();
+                previousCount = Set.items.Count;
+            }
+        }
+
+        private void UpdateText()
+        {
+            Text.text = "There are " + Set.items.Count + " things in this collection";
+        }
+    }
+}
