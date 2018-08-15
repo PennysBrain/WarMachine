f6ddd4bd803c89ba11921c50ad41b40d3f2024bd
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2018-08-15 08:38:09 -0500

    Fix Update and Mistakes uploading

---
+++ WarMachine/Assets/Modular System Base/Scripts/Events/OnClickEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace WARMachine.Events
+{
+    public class OnClickEvent : MonoBehaviour
+    {
+        [Tooltip("Event To Raise")]
+        public GameEvent Event;
+
+        private void OnMouseDown()
+        {
+            Event.Raise();
+        }
+    }
+}
